08bf1bcf9328deaadb88f697fe84eb46bbb6a5cf
Matt Gillis <gillism@purdue.edu>
2018-06-21 17:44:59 -0400

    Created the Friction Equation power up that doubles the player's movement speed.

---
+++ ProjectFolder/Sideways/Assets/Scripts/equationFriction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equationFriction : MonoBehaviour {
+
+    public GameObject player;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            Destroy(this.gameObject);
+
+            // move twice as fast
+            player.GetComponent<playerBehavior>().walkSpeed = 10;
+            player.GetComponent<playerBehavior>().runSpeed = 20;
+
+            // Smooth because dat friction makes you slick!
+            col.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+
+        }
+    }
+}
