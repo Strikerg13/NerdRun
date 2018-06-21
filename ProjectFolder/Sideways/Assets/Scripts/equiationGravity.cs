0aad2c71f2de9de89b1654862060b7f9139d230c
Matt Gillis <gillism@purdue.edu>
2018-06-21 16:04:36 -0400

    added changes

---
+++ ProjectFolder/Sideways/Assets/Scripts/equiationGravity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equiationGravity : MonoBehaviour {
+
+    public GameObject player;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+
+            Destroy(this.gameObject);
+
+            // jump twice as high
+            player.GetComponent<betterJump>().jumpVelocity = 18.0f;
+
+            // triggered cause gravity doesn't work like that!
+            col.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+
+        }
+    }
+}
