a79267bb9e118df3d75cd947ad2cd0398cae917f
Matt Gillis <gillism@purdue.edu>
2018-06-24 15:42:55 -0400

    Still fixing issues with player controls working while paused. Might redo the pause menu system entirely.

--- ProjectFolder/Sideways/Assets/Scripts/pauseGame.cs
+++ ProjectFolder/Sideways/Assets/Scripts/pauseGame.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class pauseGame : MonoBehaviour {
 
+    public static bool GameIsPaused = false;
     public Transform PauseScreen;
-
-    void Awake ()
-    {
-        highlightSelection();
-    }
 
 	// Update is called once per frame
 	void Update () 
     {
 		if (Input.GetButtonDown("Cancel")) 
         {
-            Pause();
+            if (!GameIsPaused)
+                Pause();
+            else
+                Unpause();
 		}
 
 
@@ -24,35 +23,29 @@ public class pauseGame : MonoBehaviour {
 
     public void Pause()
     {
-        if (Time.timeScale == 1)  
-        {
-            // pause time
-            Time.timeScale = 0;
+        // pause time
+        Time.timeScale = 0;
 
-            // show pause screen
-            PauseScreen.gameObject.SetActive(true);
-        }
-        else
-        {
-            // unpause time
-            Time.timeScale = 1;   
+        // show pause screen
+        PauseScreen.gameObject.SetActive(true);
 
-            // hide pause screen
-            PauseScreen.gameObject.SetActive(false);
-        }
+        GameIsPaused = true;
+    }
+
+    public void Unpause()
+    {
+        // unpause time
+        Time.timeScale = 1;   
+
+        // hide pause screen
+        PauseScreen.gameObject.SetActive(false);
+
+        GameIsPaused = false;
     }
 
     public void quitGame()
     {
         print("Player has quit.");
         Application.Quit();
-    }
-
-    public void highlightSelection()
-    {
-        GameObject myEventSystem = GameObject.Find("EventSystem");
-        myEventSystem.GetComponent<UnityEngine.EventSystems.EventSystem>().SetSelectedGameObject(GameObject.Find("btnResume"));
-
-
     }
 }
