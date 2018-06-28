c35447fcbae46303efee0a6f017be9648d49caf3
Matt Gillis <gillism@purdue.edu>
2018-06-28 16:29:36 -0400

    other files forgot to add.

---
+++ ProjectFolder/Sideways/Assets/selectionController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class selectionController : MonoBehaviour {
+
+    Button btn;
+    EventSystem es;
+
+
+    void Awake()
+    {
+        if (startMenuController.gameIsPaused)
+        {
+            btn = GameObject.Find("btnResume").GetComponent<Button>();
+            es = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+            es.SetSelectedGameObject(btn.gameObject);
+        }
+
+    }
+
+}
---
+++ ProjectFolder/Sideways/Assets/startMenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startMenuController : MonoBehaviour {
+
+    public static bool gameIsPaused = false;
+    public GameObject startMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Menu"))
+        {
+            if (gameIsPaused)
+                Resume();
+            else
+                Pause();
+        }
+	}
+
+
+    public void Resume()
+    {
+        startMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        startMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void loadMenu()
+    {
+        print("Loading Menu...");
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void QuitGame()
+    {
+        print("Player has quit.");
+        Application.Quit();
+    }
+}
