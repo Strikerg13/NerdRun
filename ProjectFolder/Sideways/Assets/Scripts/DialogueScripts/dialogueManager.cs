2b8435756060607fc0af1aa7ca24deed53391e79
Matt Gillis <gillism@purdue.edu>
2018-11-04 14:02:00 -0500

    Added a prototype doalogue system in scene3. Uses a Fedora Linux terminal as the backdrop for the text.

---
+++ ProjectFolder/Sideways/Assets/Scripts/DialogueScripts/dialogueManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour {
+
+    public Text txtName;
+    public Text txtDialogue;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+	
+	void Start () {
+        sentences = new Queue<string>();
+	}
+	
+    public void StartDialogue(dialogue Dialogue)
+    {
+        //play animation to display the dialogue box.
+        animator.SetBool("isOpen", true);
+
+        txtName.text = Dialogue.name;
+
+        //Clear any previous conversations.
+        sentences.Clear();
+
+        //go through each sentence.
+        foreach (string sentence in Dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        //show the next sentence
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        } 
+
+        //pull the next sentence out of the Queue.
+        string sentence = sentences.Dequeue();
+
+        //stop typing the previous sentence.
+        StopAllCoroutines();
+
+        //start typing the current sentence.
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        txtDialogue.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            txtDialogue.text += letter;
+
+            //wait 1 frame
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        //play animation to hide the dialogue box.
+        animator.SetBool("isOpen", false);
+    }
+}
---
+++ ProjectFolder/Sideways/Assets/Scripts/npcController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcController : MonoBehaviour {
+
+    public GameObject player;
+    public float otherObjectX;
+    public float otherObjectY;
+    float oldDistanceToObject;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello, I'm an NPC");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        GetPositionOfAnObject(player);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        GetDistanceToObject(player);
+	}
+
+    void GetPositionOfAnObject(GameObject otherObject)
+    {
+        if (otherObject != null)
+        {
+            otherObjectX = otherObject.GetComponent<Transform>().position.x;
+            otherObjectY = otherObject.GetComponent<Transform>().position.y;
+            Debug.Log("I found the player. He's at (" + otherObjectX.ToString() + "," + otherObjectY.ToString() + ")");
+        }
+    }
+
+    void GetDistanceToObject(GameObject otherObject)
+    {
+        float distanceToObject;
+        float changeInDistance;
+
+        distanceToObject = (otherObject.transform.position - this.transform.position).magnitude;
+
+        if (distanceToObject != oldDistanceToObject)
+        {
+            changeInDistance = Mathf.Abs(oldDistanceToObject - distanceToObject);
+            if (changeInDistance >= 0.5f)
+            {
+                Debug.Log("Leinad is " + distanceToObject.ToString("F2") + " away.");
+                oldDistanceToObject = distanceToObject;
+            }
+
+        }
+
+    }
+}
